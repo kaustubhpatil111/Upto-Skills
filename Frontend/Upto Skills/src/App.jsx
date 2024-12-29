df613c8792d6a52caf131de2c073d115702d7b1d
kaustubhpatil111 <kaustubh.patil_comp22@pccoer.in>
2024-12-29 15:06:09 +0530

    statiticss done

--- Frontend/Upto Skills/src/App.jsx
+++ Frontend/Upto Skills/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Footer from "./Component/Footer";
 import ExploreJobs from "./Component/Explore-Jobs";
+import StatisticsSection from "./Component/StatisticsSection";
 
 function App() {
   return (
     <>
+      <StatisticsSection />
       <ExploreJobs />
       <Footer />
     </>
