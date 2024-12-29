29375a47a03aa610405f94402160e4217a107eae
unknown <vetcherala@gmail.com>
2024-12-29 01:49:16 -0800

    section4 updated

--- Frontend/Upto Skills/src/App.jsx
+++ Frontend/Upto Skills/src/App.jsx
@@ -2,10 +2,12 @@ import "./App.css";
 import Footer from "./Component/Footer";
 import ExploreJobs from "./Component/Explore-Jobs";
 import StatisticsSection from "./Component/StatisticsSection";
+import Freetraining from "./components/FreeTraning";
 
 function App() {
   return (
     <>
+     <Freetraining />
       <StatisticsSection />
       <ExploreJobs />
       <Footer />
