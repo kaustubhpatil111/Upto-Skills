91251fdab215ebb6e2534be5883162f263bdc8fe
kaustubhpatil111 <kaustubh.patil_comp22@pccoer.in>
2024-12-29 14:31:23 +0530

    Explore Job complete

--- Frontend/Upto Skills/src/App.jsx
+++ Frontend/Upto Skills/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Footer from "./Component/Footer";
+import ExploreJobs from "./Component/Explore-Jobs";
 
 function App() {
   return (
     <>
+      <ExploreJobs />
       <Footer />
     </>
   );
---
+++ Frontend/Upto Skills/src/Component/Explore-Jobs.jsx
@@ -0,0 +1,28 @@
+import "./Explore-jobs.css";
+
+function ExploreJobs() {
+  return (
+    <div className="custom-frame">
+      <div className="content">
+        <div className="text-content">
+          <h2>Explore New Jobs Now</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur. Viverra lacinia sed praesent
+            pharetra quisque consectetur tellus elit. Suspendisse ultrices vitae
+            at platea. Euismod pretium sit est etiam.
+          </p>
+          <button>Get Started</button>
+        </div>
+        <div className="image-container">
+          <img
+            src="/ExploreJob.png"
+            alt="Explore New Jobs Now"
+            className="woman-image"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ExploreJobs;
