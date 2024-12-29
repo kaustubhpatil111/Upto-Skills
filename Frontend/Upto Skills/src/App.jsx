97c71023024cba274dd7f2f258fb374474d1588c
Kundan Kumar <rajkundan9535@gmail.com>
2024-12-29 13:24:21 +0530

    "Course Section update"

--- Frontend/Upto Skills/src/App.jsx
+++ Frontend/Upto Skills/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import CourseCard from "./components/CourseCard";
+import { CourseSection } from "./components/CourseSection";
+import cardImage1 from "./assets/Image/card1.png";
+import cardImage2 from "./assets/Image/card2.png";
+import cardImage3 from "./assets/Image/card4.png";
+import cardImage4 from "./assets/Image/card3.png";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex items-center justify-between py-16 px-8 text-white m-4 rounded-xl">
+        <CourseCard
+          imageSrc={cardImage1}
+          imageAlt="Noteworthy technology acquisitions"
+          title="Product Management Basic - Course"
+          description="Product Management Masterclass, you will learn with Sarah Johnson - Head of Product Customer Platform Gojek Indonesia."
+        />
+        <CourseCard
+          imageSrc={cardImage2}
+          imageAlt="Noteworthy technology acquisitions"
+          title="BM Data Science Professional Certificate"
+          description="Product Management Masterclass, you will learn with Sarah Johnson - Head of Product Customer Platform Gojek Indonesia."
+        />
+        <CourseCard
+          imageSrc={cardImage3}
+          imageAlt="Noteworthy technology acquisitions"
+          title="The Science of Well-Being"
+          description="Product Management Masterclass, you will learn with Sarah Johnson - Head of Product Customer Platform Gojek Indonesia."
+        />
+        <CourseCard
+          imageSrc={cardImage4}
+          imageAlt="Noteworthy technology acquisitions"
+          title="Python for Everybody Specialization"
+          description="Product Management Masterclass, you will learn with Sarah Johnson - Head of Product Customer Platform Gojek Indonesia."
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <CourseSection />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
